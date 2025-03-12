13fe42028bf3a4b4386b2faf907d481435690845
Alexandre Matias <axdmatias310@gmail.com>
2025-03-11 21:26:16 -0300

    finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function App({ Component }:any) {
-  return <Component  />;
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import logo from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+import Image from 'next/image'
+
+globalStyles()
+ 
+export default function App({ Component, pageProps}:AppProps) {
+  return(
+    <Container>
+    <Header>
+      <Image src={logo} alt="" />
+    </Header>
+    <Component {...pageProps}  />
+    </Container>
+
+  ) 
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,107 @@
-import { useRouter } from "next/router"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { stripe } from "@/src/lib/stripe";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/products";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Stripe from "stripe";
 
-export default function Product(){
-    const { query } = useRouter()
- 
-    return(
-        <h1>Product: {JSON.stringify(query)}</h1>
-    )
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string
+  };
 }
+
+export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckout, setIsCreatingCheckout] = useState(false)
+    const { isFallback } = useRouter();
+
+    if (isFallback) {
+      return <h1>Carregando...</h1>;
+    }
+
+    
+    async function handleBuyProduct(){
+        //const router = useRouter()
+
+        try{
+            setIsCreatingCheckout(true)
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId
+            })
+
+
+            const { checkoutUrl } = response.data;
+
+            //router.push(checkoutUrl)
+
+
+            window.location.href = checkoutUrl;
+        }catch{
+            alert('Falha ao redirecionar')
+        }
+    }
+
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button disabled={isCreatingCheckout} onClick={handleBuyProduct}>Comprar</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params!.id;
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hours
+  };
+};
